6fdb086426b7398ab33aa61ebd33a43bc38a01ca
Kosh42 <kosh42efg@gmail.com>
2020-12-17 21:42:47 +0000

    Pixels and current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2329,7 +2329,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       700
+    #define Z_CURRENT       750
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2365,7 +2365,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      650
+    #define E0_CURRENT      700
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
